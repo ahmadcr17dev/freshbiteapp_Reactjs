9cdd93f4ec4fdf2c4a7de786f9df22ab4f402518
Muhammad Ahmad Mobeen <chaudaryahmad1041@gmail.com>
2024-09-13 12:09:09 +0500

    Done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,8 @@ import { FaTrash } from 'react-icons/fa';
 import toast from 'react-hot-toast';
 import { NavLink } from 'react-router-dom';
 import { MoonLoader } from 'react-spinners';
+import { FaPlus } from 'react-icons/fa';
+import { FaMinus } from 'react-icons/fa';
 
 const Styledsection = styled.section`
     display: block;
@@ -84,7 +86,7 @@ const Styledsection = styled.section`
             color: gray;
         }
     }
-    #lowerbox {
+    .lowerbox {
         border: 1px solid #898989;
         border-radius: 1rem;
         margin: 3rem 0rem 1rem 0rem;
@@ -116,6 +118,85 @@ const Styledsection = styled.section`
                 left: 30%;
             }
         }
+        #tablet {
+            display: none;
+        }
+    }
+    @media (max-width: 899px) and (min-width: 600px) {
+        display: none;
+    }
+`;
+const Styledtab = styled.div`
+    @media (max-width: 1990px) and (min-width: 900px) {
+        display: none;
+    }
+    @media (max-width: 899px) and (min-width: 600px) {
+        display: block;
+        border: 1px solid #898989;
+        margin: 5rem auto 3rem auto;
+        width: 90%;
+        border-radius: 1rem;
+        padding: 1rem;
+        font-family: "poppins", sans-serif;
+        #innerbox {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        #imagename {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            img {
+                width: 8rem;
+                height: 8rem;
+            }
+            h5 {
+                margin: 3rem 0rem 0rem 0rem;
+            }
+            p {
+                font-weight: 500;
+                color: #696969;
+            }
+        }
+        #secondbox {
+            div {
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+            #label {
+                margin: 1.3rem 0rem 0rem 0rem;
+                p {
+                    padding: .8rem .8rem 0rem .8rem;
+                    text-align: center;
+                    font-weight: 500;
+                    color: #696969;
+                }
+            }
+            #items {
+                p {
+                    padding: 0rem .8rem 0rem .8rem;
+                    text-align: center;
+                }
+            }
+            #button {
+            display: flex;
+            padding: 0rem 1rem 0rem 0rem;
+            button {
+                margin: 0rem 0rem 0rem 0rem;
+                border: 0px solid black;
+                height: 1rem;
+            }
+            input {
+                width: 1rem;
+                height: 2.5rem;
+                padding: auto;
+                margin: 1.3rem 0rem 0rem 0rem;
+                border: 0px solid black;
+            }
+        }
+        }
     }
 `;
 
@@ -126,7 +207,6 @@ const Cart = () => {
     const cartitems = useSelector((state) => state.cart.items);
     const subtotal = useSelector(selectCartSubtotal);
     const totalitems = useSelector(totalquantity);
-
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -169,52 +249,109 @@ const Cart = () => {
             {loading ? (
                 <p className='container d-flex justify-content-center align-items-center' style={{ marginTop: "5rem" }}> <MoonLoader size={60} color={"red"} /> </p>
             ) : (
-                <Styledsection>
-                    <div id='labels'>
-                        <p>Product</p>
-                        <p>Price</p>
-                        <p>Quantity</p>
-                        <p>SubTotal</p>
-                        <p>Remove</p>
-                    </div>
-                    <div>
-                        {cartitems.length === 0 ? (
-                            <p>Your cart is empty.</p>
-                        ) : (
-                            <>
-                                <div>
-                                    {cartitems.map((item) => (
-                                        <div id='items' key={item.name}>
-                                            <div id='itemname'>
-                                                <img src={item.imageurl} alt='Product Pic' />
-                                                <p>
-                                                    {item.name} <br />
-                                                    {item.weigh && <p className='quantity'>{item.weigh} kg</p>}
-                                                    {item.dozen && <p className='quantity'>{item.dozen} dozen</p>}
-                                                    {item.bunch && <p className='quantity'>{item.bunch} bunch</p>}
-                                                    {item.size && <p className='quantity'>{item.size}</p>}
-                                                </p>
-                                            </div>
-                                            <p id='price'>${item.price}</p>
-                                            <div id='buttons'>
-                                                <button className='btn' onClick={() => handledecrement(item)} disabled={item.quantity <= 1} >-</button>
-                                                <p>
-                                                    {item.quantity}
-                                                </p>
-                                                <button className='btn' onClick={() => handleincrement(item)} disabled={item.quantity >= item.stock} >+</button>
-                                            </div>
-                                            <p id='subtotal'>${parseFloat(item.quantity * item.price).toFixed(2)}</p>
-                                            <FaTrash onClick={() => handleRemove(item)} color={'red'} id='trashicon' />
-                                        </div>
-                                    ))}
-                                    <button className='btn btn-danger mt-5' onClick={handleclearcart}>Clear Cart</button>
-                                </div>
-                                <div id='lowerbox'>
+                <>
+                    <Styledsection>
+                        <div id='labels'>
+                            <p>Product</p>
+                            <p>Price</p>
+                            <p>Quantity</p>
+                            <p>SubTotal</p>
+                            <p>Remove</p>
+                        </div>
+                        <div>
+                            {cartitems.length === 0 ? (
+                                <p>Your cart is empty.</p>
+                            ) : (
+                                <>
                                     <div>
+                                        {cartitems.map((item) => (
+                                            <div id='items' key={item.name}>
+                                                <div id='itemname'>
+                                                    <img src={item.imageurl} alt='Product Pic' />
+                                                    <p>
+                                                        {item.name} <br />
+                                                        {item.weigh && <p className='quantity'>{item.weigh} kg</p>}
+                                                        {item.dozen && <p className='quantity'>{item.dozen} dozen</p>}
+                                                        {item.bunch && <p className='quantity'>{item.bunch} bunch</p>}
+                                                        {item.size && <p className='quantity'>{item.size}</p>}
+                                                    </p>
+                                                </div>
+                                                <p id='price'>${item.price}</p>
+                                                <div id='buttons'>
+                                                    <button className='btn' onClick={() => handledecrement(item)} disabled={item.quantity <= 1} >-</button>
+                                                    <p>
+                                                        {item.quantity}
+                                                    </p>
+                                                    <button className='btn' onClick={() => handleincrement(item)} disabled={item.quantity >= item.stock} >+</button>
+                                                </div>
+                                                <p id='subtotal'>${parseFloat(item.quantity * item.price).toFixed(2)}</p>
+                                                <FaTrash onClick={() => handleRemove(item)} color={'red'} id='trashicon' />
+                                            </div>
+                                        ))}
+                                        <button className='btn btn-danger mt-5' onClick={handleclearcart}>Clear Cart</button>
+                                    </div>
+                                    <div className='lowerbox'>
+                                        <div>
+                                            <h6>Total items: </h6>
+                                            <p>{totalitems} items</p>
+                                        </div>
+                                        <div>
+                                            <h4>Total</h4>
+                                            <p>${parseFloat(subtotal).toFixed(2)}</p>
+                                        </div>
+                                        <button className='container btn btn-danger' onClick={handlecheckout}>
+                                            <NavLink to="/checkout" className='container text-decoration-none text-white'>Prodeed to checkout</NavLink>
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </Styledsection>
+                    <Styledtab id='tablet'>
+                        {cartitems.length === 0 ? (
+                            <p>Your Cart is Empty!</p>
+                        ) : (
+                            <div className='container'>
+                                {cartitems.map((item) => (
+                                    <div key={item.id} id='innerbox'>
+                                        <div id='imagename'>
+                                            <img src={item.imageurl} alt={item.name} />
+                                            <div>
+                                                <h5>{item.name}</h5>
+                                                {item.weigh && <p className='quantity'>{item.weigh} kg</p>}
+                                                {item.dozen && <p className='quantity'>{item.dozen} dozen</p>}
+                                                {item.bunch && <p className='quantity'>{item.bunch} bunch</p>}
+                                                {item.size && <p className='quantity'>{item.size}</p>}
+                                            </div>
+                                        </div>
+                                        <div id='secondbox'>
+                                            <div id='label'>
+                                                <p>Price</p>
+                                                <p style={{ margin: '0rem 1.5rem 0rem 1.5rem' }}>Quantity</p>
+                                                <p>SubTotal</p>
+                                            </div>
+                                            <div id='items'>
+                                                <p>${parseFloat(item.price).toFixed(2)}</p>
+                                                <div id='button'>
+                                                    <button className='btn' onClick={() => handledecrement(item)} disabled={item.quantity <= 1} >-</button>
+                                                    <p>
+                                                        {item.quantity}
+                                                    </p>
+                                                    <button className='btn' onClick={() => handleincrement(item)} disabled={item.quantity >= item.stock} >+</button>
+                                                </div>
+                                                <p>${parseFloat(item.price * item.quantity).toFixed(2)}</p>
+                                                <FaTrash style={{ marginRight: '-15px', marginTop: '5px' }} color={'red'} size={".8rem"} onClick={() => handleRemove(item)} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                <button className='btn btn-danger mt-5' onClick={handleclearcart}>Clear Cart</button>
+                                <div className='lowerbox' style={{ marginTop: '3rem' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                                         <h6>Total items: </h6>
                                         <p>{totalitems} items</p>
                                     </div>
-                                    <div>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                                         <h4>Total</h4>
                                         <p>${parseFloat(subtotal).toFixed(2)}</p>
                                     </div>
@@ -222,10 +359,10 @@ const Cart = () => {
                                         <NavLink to="/checkout" className='container text-decoration-none text-white'>Prodeed to checkout</NavLink>
                                     </button>
                                 </div>
-                            </>
+                            </div>
                         )}
-                    </div>
-                </Styledsection>
+                    </Styledtab>
+                </>
             )}
         </>
     );
