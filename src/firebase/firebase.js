e9ce2e78f93bf6bc8cbd25840b4c9d2dc0e360f3
Muhammad Ahmad Mobeen <chaudaryahmad1041@gmail.com>
2024-09-13 22:28:47 +0500

    Done

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,13 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUoMzD8fdip3Oog3rb4jFLdS4vTHqybuI",
-  authDomain: "estore-react-922c8.firebaseapp.com",
-  projectId: "estore-react-922c8",
-  storageBucket: "estore-react-922c8.appspot.com",
-  messagingSenderId: "36464998952",
-  appId: "1:36464998952:web:69254f1256a6d77180131d",
-  // databaseURL: "https://estore-react-922c8-default-rtdb.firebaseio.com/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
